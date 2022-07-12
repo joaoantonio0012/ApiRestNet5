739fcfc41b43459678446ebb1e3ec579fb7febc8
João <joaoantoniobmx@hotmail.com>
2022-07-12 18:07:35 -0300

    Criado controller de pessoa

---
+++ 02_ApiRestNet5VisualStudio/ApiRestNet5/ApiRestNet5/Controllers/PessoaController.cs
@@ -0,0 +1,46 @@
+﻿using ApiRestNet5.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace ApiRestNet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+         
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("index/")]
+        public IActionResult Index( )
+        {
+
+            PessoaModel pessoa   = new PessoaModel
+            {
+                Nome = "João Antonio ",
+                SobreNome = "Amaro Pereira",
+                Endereco = "Rua Pedro Carlos de souza 278 Jardim Europa Rancharia Sp",
+                Genero = "Masculino",
+                Id = 1
+            };
+            var retorno = JsonSerializer.Serialize(pessoa);
+            return Ok(retorno);
+            
+        } 
+        
+        [HttpPost("cadastrar/")]
+        public IActionResult Cadastrar(PessoaModel pessoa )
+        {
+            return Ok();
+            
+        } 
+        
+       
+    }
+}
---
+++ 02_ApiRestNet5VisualStudio/ApiRestNet5/ApiRestNet5/Model/PessoaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestNet5.Model
+{
+    public class PessoaModel
+    {
+
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Endereco { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
